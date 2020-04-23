655790fdc2959dc72df4c0fc61e4232085ae44ea
Rushed_up <bhayani3@illinois.edu>
2020-04-24 01:22:11 +0530

    Removing the icons and reverted back to swipe functionality + normal tab names because that's more sleek and easier to design. refer to https://cs125-forum.cs.illinois.edu/t/working-with-tabbed-layout-in-android-studio/23821/6

--- app/src/main/java/com/project/wallpaperportal/MainActivity.java
+++ app/src/main/java/com/project/wallpaperportal/MainActivity.java
@@ -7,10 +7,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 import com.project.wallpaperportal.logic.Flickr;
-
+import com.project.wallpaperportal.ui.main.SectionsPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.Flickr).setOnClickListener(unused -> startActivity(new Intent(this, Flickr.class)));
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+//        findViewById(R.id.Flickr).setOnClickListener(unused -> startActivity(new Intent(this, Flickr.class)));
     }
 }
---
+++ app/src/main/java/com/project/wallpaperportal/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.project.wallpaperportal.ui.main;
+
+import androidx.fragment.app.FragmentPagerAdapter;
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+//import androidx.fragment.app.FragmentPagerAdapter;
+import com.project.wallpaperportal.R;
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.flickr, R.string.spotify, R.string.nasa};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+}
