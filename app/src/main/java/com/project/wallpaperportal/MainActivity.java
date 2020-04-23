d020f7a24e6b4d90344252160d5a79b69079bb4f
Rushed_up <bhayani3@illinois.edu>
2020-04-23 21:52:28 +0530

    I don't even know what I did at this point, created Flickr.java for flickr page logic ?

--- app/src/main/java/com/project/wallpaperportal/MainActivity.java
+++ app/src/main/java/com/project/wallpaperportal/MainActivity.java
@@ -1,10 +1,12 @@
 package com.project.wallpaperportal;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
+import com.project.wallpaperportal.logic.Flickr;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.Flickr).setOnClickListener(unused -> startActivity(new Intent(this, Flickr.class)));
     }
 }
---
+++ app/src/main/java/com/project/wallpaperportal/logic/Flickr.java
@@ -0,0 +1,12 @@
+package com.project.wallpaperportal.logic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Flickr extends AppCompatActivity {
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
