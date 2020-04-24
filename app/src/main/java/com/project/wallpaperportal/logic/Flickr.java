d2de9729f3e4dafbc7512fe44dbfa66a461b9fe3
Rushed_up <bhayani3@illinois.edu>
2020-04-24 22:29:15 +0530

    Added starter code for Flickr tab, enjoy @Abraham, because this is probably the last help I can give

--- app/src/main/java/com/project/wallpaperportal/logic/Flickr.java
+++ app/src/main/java/com/project/wallpaperportal/logic/Flickr.java
@@ -1,9 +1,14 @@
 package com.project.wallpaperportal.logic;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 //import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import com.project.wallpaperportal.R;
 
 public class Flickr extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
@@ -14,8 +19,15 @@ public class Flickr extends Fragment {
         fragment.setArguments(bundle);
         return fragment;
     }
+    @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+    }
+    //refer to https://www.androidhive.info/2015/09/android-material-design-working-with-tabs/
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.flickr_tab, container, false);
     }
 }
--- app/src/main/java/com/project/wallpaperportal/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/project/wallpaperportal/ui/main/SectionsPagerAdapter.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 //import androidx.fragment.app.FragmentPagerAdapter;
 import com.project.wallpaperportal.R;
+import com.project.wallpaperportal.logic.Flickr;
+
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @StringRes
     private static final int[] TAB_TITLES = new int[]{R.string.flickr, R.string.spotify, R.string.nasa};
@@ -21,9 +23,18 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
+        switch (position) {
+            case 1:
+                return PlaceholderFragment.newInstance(position + 19);
+            case 2:
+                return PlaceholderFragment.newInstance(position + 28);
+            default:
+                return Flickr.newInstance(position + 1);
+        }
+
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+//        return PlaceholderFragment.newInstance(position + 1);
     }
 
     @Nullable
