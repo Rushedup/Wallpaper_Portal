bd0f9eb792fbd43089732b25e65c3946fe9aaca5
Rushed_up <bhayani3@illinois.edu>
2020-05-01 21:45:17 +0530

    Able to receive clusters of flickr tags.

--- app/src/main/java/com/project/wallpaperportal/logic/Flickr.java
+++ app/src/main/java/com/project/wallpaperportal/logic/Flickr.java
@@ -1,36 +1,46 @@
 package com.project.wallpaperportal.logic;
 import android.app.WallpaperManager;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import androidx.fragment.app.Fragment;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.project.wallpaperportal.R;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Flickr extends Fragment {
     private Bitmap image;
+    private TextView failedView;
+    private ImageView imageView;
     private static final String ARG_SECTION_NUMBER = "section_number";
     private final String api_key = "84847469a610a5f8c906b1ef3e4321b5";
-    private String getClusterUrl;
+    private String keyword = null;
+//    private
     private String getClusterPhotos;
     private String getPhotoSizes;
     private  String test = "https://www.flickr.com/services/rest/?method=flickr.test.echo&name=value" +
-            "&api_key=84847469a610a5f8c906b1ef3e4321b5&format=json&nojsoncallback=1";
+            "&api_key="+api_key+"&format=json&nojsoncallback=1";
     public static Flickr newInstance(int index) {
         Flickr fragment = new Flickr();
         Bundle bundle = new Bundle();
@@ -40,35 +50,46 @@ public class Flickr extends Fragment {
     }
     @Override
     public void onCreate(final Bundle savedInstanceState) {
-        System.out.println("flickr on create");
         super.onCreate(savedInstanceState);
-        System.out.println("flickr on create");
     }
     //refer to https://www.androidhive.info/2015/09/android-material-design-working-with-tabs/
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        System.out.println("flickr on create view");
         View root = inflater.inflate(R.layout.flickr_tab, container, false);
-        callFlickr(test);
+        imageView = root.findViewById(R.id.flickr_image_view);
+        imageView.setVisibility(View.GONE);
+        failedView = root.findViewById(R.id.failedView);
+        failedView.setVisibility(View.GONE);
+        FloatingActionButton searchButton = root.findViewById(R.id.search_button);
+        TextInputEditText searchBar = root.findViewById(R.id.search_bar);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String key = searchBar.getText().toString();
+                keyword = key;
+                System.out.println(keyword);
+                callFlickr(key);
+            }
+        });
         return root;
     }
-    private void callFlickr (String url) {
+    private void callFlickr (String keyword) {
+        failedView.setVisibility(View.GONE);
         RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        String getCluster = "https://www.flickr.com/services/rest/?method=flickr.tags." +
+                "getClusters&api_key="+api_key+"&tag="+keyword+"&format=json&nojsoncallback=1";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.GET,
-                url,
+                getCluster,
                 null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        try{
-                            JSONObject jsonObject = response.getJSONObject("api_key");
-                            String print = jsonObject.getString("_content");
-                            System.out.println(print);
-                        }catch (JSONException e){
-                            //print the error
+                        try {
+                            getTagCluster(response);
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
@@ -102,5 +123,38 @@ public class Flickr extends Fragment {
                 setWallapaper(image);
             }
         });
+    }
+    private void getTagCluster(JSONObject response) throws JSONException {
+        if (response.getString("stat").equals("fail")) {
+            failedView.setVisibility(View.VISIBLE);
+        }
+        JSONObject clusters = response.getJSONObject("clusters");
+        JSONArray clustersJSONArray = clusters.getJSONArray("cluster");
+        List<String> clusterTags = new ArrayList<String>();
+        int clustersLength = clustersJSONArray.length();
+        if (clustersLength >= 25) {
+            clustersLength = 25;
+        }
+        for (int i = 0; i < clustersLength; i++) {
+            JSONObject temp = clustersJSONArray.getJSONObject(i);
+            int total = temp.getInt("total");
+            if (total >= 10) {
+                total = 10;
+            }
+            JSONArray tagArray = temp.getJSONArray("tag");
+            for(int j = 0; j < total; j++) {
+                JSONObject tagArrayJSONObject = tagArray.getJSONObject(j);
+                String tag = tagArrayJSONObject.getString("_content");
+                clusterTags.add(tag);
+            }
+        }
+        System.out.println(clusterTags.size());
+        String[] tags = getClusterPhotoOptions(clusterTags);
+
+    }
+    private String[] getClusterPhotoOptions (List<String> tags) {
+        String[] tagOptions = new String[3];
+        //get any 3 tags from the given List of String.
+        return  tagOptions;
     }
 }
--- app/src/main/java/com/project/wallpaperportal/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/project/wallpaperportal/ui/main/SectionsPagerAdapter.java
@@ -7,11 +7,9 @@ import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-//import androidx.fragment.app.FragmentPagerAdapter;
 import com.project.wallpaperportal.R;
 import com.project.wallpaperportal.logic.Flickr;
 import com.project.wallpaperportal.logic.NASA;
-import com.project.wallpaperportal.logic.Spotify;
 
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @StringRes
@@ -25,24 +23,12 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        /*switch (position) {
-            case 1:
-                return Spotify.newInstance(position + 19);
-            case 2:
-                return NASA.newInstance(position + 28);
-            default:
-                return Flickr.newInstance(position + 1);
-        }*/
+
         if (position == 0) {
             return  Flickr.newInstance(position);
         } else {
             return  NASA.newInstance(position);
         }
-
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-//        return PlaceholderFragment.newInstance(position + 1);
-//        return null;
     }
 
     @Nullable
